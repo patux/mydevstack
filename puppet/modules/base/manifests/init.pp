3aee8ce42d6c13744b6dafb85d31869cc5b0954c
Geronimo Orozco <geronimo.orozco@intel.com>
2014-04-15 15:57:21 -0500

    Make explicit the file dependency to install setup_tools

--- puppet/modules/base/manifests/init.pp
+++ puppet/modules/base/manifests/init.pp
@@ -30,7 +30,7 @@ class base {
       group   => root,
       mode    => 600,
       source  => 'puppet:///modules/base/setuptools-1.1.tar.gz',
-  } 
+  } ->
   exec { "/bin/bash -c 'source /etc/environment;cd /tmp;/bin/tar zxvf setuptools-1.1.tar.gz;cd setuptools-1.1 && sudo python setup.py install;sudo easy_install pip'":
     timeout   => 0,
     logoutput => true,
