bfd22bfaf745bebfd4c7941b773130f5036a373e
Geronimo Orozco <geronimo.orozco@intel.com>
2014-07-20 18:10:07 -0500

    Simplify setup, update proxy reference

--- puppet/modules/base/manifests/init.pp
+++ puppet/modules/base/manifests/init.pp
@@ -1,5 +1,5 @@
 class base {
-  $base_packages = [ "curl", "git-core", "vim", "wget", "libssl0.9.8", "build-essential", "libsqlite3-dev", "libffi-dev", "git-review", "python-dev" ]
+  $base_packages = [ "curl", "git-core", "vim", "wget", "build-essential", "git-review", "python-dev"]
 
   exec {"hold kernel":
     command   => "/usr/bin/apt-mark hold linux-image-`uname -r`; echo ",
@@ -24,18 +24,4 @@ class base {
 
   class { "aptitude": require         => Exec['update apt'], } ->
   package { $base_packages: ensure => installed }
-    file { '/tmp/setuptools-1.1.tar.gz':
-      ensure  => file,
-      owner   => root,
-      group   => root,
-      mode    => 600,
-      source  => 'puppet:///modules/base/setuptools-1.1.tar.gz',
-  } ->
-  exec { "/bin/bash -c 'source /etc/environment;cd /tmp;/bin/tar zxvf setuptools-1.1.tar.gz;cd setuptools-1.1 && sudo python setup.py install;sudo easy_install pip'":
-    timeout   => 0,
-    logoutput => true,
-    require   => Package[$base_packages],
-  }
-
-
 }
